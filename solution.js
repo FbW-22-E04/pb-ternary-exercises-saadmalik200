72b08667699c01cac60dfaec8a10b2a7818ec29a
Saad Malik <saad_malik200@hotmail.com>
2022-07-11 17:41:05 +0200

    ex 3

---
+++ solution.js
@@ -0,0 +1,38 @@
+const isDog = false;
+console.log(isDog == true ? "pat, pat" : "find me a dog to pat");
+
+console.log("-----------------------");
+
+const speedLimit = 50;
+const yourSpeed = 60;
+
+console.log(
+  yourSpeed > speedLimit
+    ? "you're going too fast!"
+    : "You're driving below the speed limit, Oma"
+);
+
+console.log("-----------------------");
+
+const age = 30;
+console.log(age < 16 ? "Serve butter beer" : "serve beer");
+
+console.log("-----------------------");
+
+const isStudent = true;
+console.log(isStudent === true ? "Ticket costs €5,00" : "Ticket costs €12,00");
+
+console.log("-----------------------");
+
+const okMarie = "cake";
+console.log(okMarie === "cake" ? "Let them eat cake" : "Oh, bother");
+
+console.log("-----------------------");
+
+const num1 = 30;
+const num2 = 939;
+const num3 = 40.9;
+
+console.log(num1 % 2 === 0 ? `${num1} is even` : `${num1} is odd`);
+console.log(num2 % 2 === 0 ? `${num2} is even` : `${num2} is odd`);
+console.log(num3 % 2 === 0 ? `${num3} is even` : `${num3} is odd`);
